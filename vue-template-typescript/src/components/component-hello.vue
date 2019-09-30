ffd6bb6290560cb7bd69534756ffddde7ab0d17a
ludeshan <deshanlu@hengtiansoft.com>
2019-09-30 16:12:10 +0800

    z自定义修饰符:zap:

--- vue-template-typescript/src/components/component-hello.vue
+++ vue-template-typescript/src/components/component-hello.vue
@@ -5,16 +5,24 @@
 </div>
 </template>
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
-
+// import Vue from 'vue'
+import Component, { mixins } from 'vue-class-component'
+import MyMixin from './mixin'
+import { NoCache } from './decorators'
 @Component({
-  props: {
-    name: String
+	props: {
+		name: String
 	}
 })
-export default class hello extends Vue {
-
+export default class hello extends mixins(MyMixin) {
+  created () {
+    console.log(this.mixinValue) // -> Hello
+	}
+	
+  @NoCache
+  get random () {
+    return Math.random()
+  }
 }
 </script>
 <style scoped>
