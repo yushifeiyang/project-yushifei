a7ade43df451e19a8211df71d2d4d8506b9be88a
ludeshan <deshanlu@hengtiansoft.com>
2019-09-30 15:26:44 +0800

    k新增typescript版本vue项目模板:white_check_mark:

---
+++ vue-template-typescript/src/components/component-hello.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+	hello component
+	<p>i am {{name}}</p>
+</div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+  props: {
+    name: String
+	}
+})
+export default class hello extends Vue {
+
+}
+</script>
+<style scoped>
+</style>
