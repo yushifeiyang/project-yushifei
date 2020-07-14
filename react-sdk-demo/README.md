a2260a4975f2cd4b5022b517c6c059da9ba09e36
云阳间 <1114744193@qq.com>
2020-07-14 17:35:16 +0800

    删除多余的重复的项目

--- react-sdk-demo/README.md
+++
@@ -1,4 +0,0 @@
-## 用到的技术
-用reacr-cli做基础，快速新建项目
-* 配置路由 HsshRouter
-
--- react-sdk-demo/src/Redux/index.js
+++
@@ -1,8 +0,0 @@
-import {createStore} from 'redux'
-import reducers from './reducers/reducers.js'
-
-let store = createStore(
-	reducers
-)
-
-export default store
