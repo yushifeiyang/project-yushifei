9d90a8887b87eb281587516b24c479939b6fbc55
ludeshan <ludeshan@163.com>
2020-12-22 12:56:06 +0800

    dialog

---
+++ elementUI-demo/src/components/dialog/index-basic.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+
+    <el-button type="text" @click="centerDialogVisible = true">点击打开 Dialog</el-button>
+
+    <el-dialog
+    title="提示"
+    :visible.sync="centerDialogVisible"
+    width="30%"
+    center
+    :destroy-on-close="true"
+    :append-to-body="true"
+    @close="closeFn">
+    <span>需要注意的是内容是默认不居中的</span>
+    <span slot="footer" class="dialog-footer">
+        <el-button @click="centerDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+    </span>
+    </el-dialog>
+
+</div>
+</template>
+<script>
+export default {
+    components: {},
+    data () {
+        return {
+            centerDialogVisible: false
+        };
+    },
+    watch: {},
+    computed: {},
+    mounted () {},
+    methods: {
+        closeFn(){
+            console.log('closeFn');
+        }
+    }
+};
+</script>
+<style lang="less" scoped>
+</style>
--- elementUI-demo/src/components/dialog/index.vue
+++ elementUI-demo/src/components/dialog/index.vue
@@ -1,31 +1,26 @@
 <template>
 <div>
+    <index-basic></index-basic>
+    <index-outer-inner></index-outer-inner>
 
-<el-button type="text" @click="centerDialogVisible = true">点击打开 Dialog</el-button>
-
-<el-dialog
-  title="提示"
-  :visible.sync="centerDialogVisible"
-  width="30%"
-  center
-  @close="closeFn">
-  <span>需要注意的是内容是默认不居中的</span>
-  <span slot="footer" class="dialog-footer">
-    <el-button @click="centerDialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
-  </span>
-</el-dialog>
 
 </div>
 </template>
 <script>
 import { log } from 'util';
+
+import indexBasic from './index-basic.vue';
+import indexOuterInner from './index-outer-inner.vue';
+
 export default {
     name:'vDialog',
-    components: {},
+    components: {
+        'index-basic': indexBasic,
+        'index-outer-inner': indexOuterInner
+    },
     data () {
         return {
-            centerDialogVisible: false
+            
         };
     },
     watch: {},
@@ -34,9 +29,6 @@ export default {
         log('11111');
     },
     methods: {
-        closeFn(){
-            console.log('closeFn');
-        }
     }
 };
 </script>
