804487155ebdbd3542103561a85bbe6c18c55910
DESKTOP-K6ETEML\ThinkPad <yushifeiyang@gmail.com>
2021-01-26 13:44:26 +0800

    新增tree自定义

---
+++ elementUI-demo/src/components/tree/index.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <el-row>
+    <el-col :span="24">
+      <basic></basic>
+    </el-col>
+  </el-row>
+</div>
+
+</template>
+<script>
+import basic from './basic.vue';
+
+export default {
+  name:'vTree',
+  components: {
+    "basic": basic
+  },
+  data() {
+    return {
+
+    };
+  },
+  watch: {
+  },
+  computed: {},
+  mounted() {},
+  methods: {
+  },
+};
+</script>
+<style lang="less" scoped>
+</style>
